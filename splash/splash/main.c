900baefafb773d1f01d26c641aeac66f3a88b5a6
Cici <aito.kenza@icloud.com>
2023-02-22 14:39:09 +0100

    ajout SDL

---
+++ splash/splash/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  splash
+//
+//  Created by Cecilia Ait Ouazzou on 21/02/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
